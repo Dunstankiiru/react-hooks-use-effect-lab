51933fc44b5477ad0aa9d134730d58ea23bfd3c0
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-04-15 19:34:38 +0300

    completes lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (timeRemaining > 1) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        onAnswered(false);
+      }
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
